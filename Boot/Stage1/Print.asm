d6e99ee8dd5fbb1996fc6db37fcea5a718a534ee
Averi <averyb.personal@gmail.com>
2025-12-28 20:51:41 -0500

    Ed0.0x04
    Null Terminator based printing for real mode.

---
+++ Boot/Stage1/Print.asm
@@ -0,0 +1,38 @@
+BITS 16
+
+MyaR_Print:
+    push ax
+    push si
+
+.Next:
+    lodsb
+    test al, al
+
+    jz .Done
+
+    mov ah, 0x0E
+    mov bh, 0x00
+
+    int 0x10
+
+    jmp .Next
+
+.Done:
+    pop si
+    pop ax
+
+    ret
+
+MyaR_PrintLine:
+    call MyaR_Print
+
+    mov al, 13
+    mov ah, 0x0E
+
+    int 0x10
+
+    mov al, 10
+    
+    int 0x10
+    
+    ret
